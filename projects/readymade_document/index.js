0ef8ed723ee8b813464b9ba2671f2a9cf415cdc1
NEXT <NEXT@NEXTui-MacBook-Pro.local>
2016-05-08 17:29:01 +0900

    [prototype] roboto font not work- excluded

--- projects/readymade_document/index.js
+++ projects/readymade_document/index.js
@@ -1,5 +1,7 @@
 $( document ).ready(function() {
-  button.on("click", function(){
-    $("#print_name").val($("#name").val());
+  $("button").on("click", function(e){
+    e.preventDefault();
+    $("#print_name").text($("#name").val());
+    window.print();
   });
 });
